4336d2ee9c343e5054d9d53d770485f7478871b7
Monxks <mnk45gamer@gmail.com>
2026-02-11 20:31:26 +0100

    Hito 3 (con clarity)

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import App from "./App";
 import './main.css'
+import Clarity from '@microsoft/clarity';
 
 const root = document.getElementById("root");
+const projectId = "vfumm138kt"
+
+Clarity.init(projectId);
 
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
